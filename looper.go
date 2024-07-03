6d9ac0063beac87a29e81e38edf4a6ebcaf6f715
Pavel Smejkal <pavel.smejkal@outreach.io>
2024-07-03 14:11:47 +0200

    feat: asyncrunner and race

---
+++ looper.go
@@ -0,0 +1,40 @@
+// Copyright 2024 Outreach Corporation. All Rights Reserved.
+
+// Description: This file contains looper structs
+package plumber
+
+import "context"
+
+// BaseLooper is a looper struct that can be used in composition as following example:
+//
+//	type Looper struct {
+//		    *plumber.BaseLooper
+//	}
+//
+//	s := &Looper{}
+//	s.BaseLooper = plumber.NewBaseLooper(s.loop)
+//
+// func (s *Looper) loop(ctx context.Context, l *plumber.Loop) error {
+// ....
+// }
+type BaseLooper struct {
+	runner RunnerCloser
+}
+
+// Run executes runners workload. Pipelines are starting Run method in separated goroutine.
+// Runner must report its readiness using given callback
+func (l *BaseLooper) Run(ctx context.Context, ready ReadyFunc) error {
+	return l.runner.Run(ctx, ready)
+}
+
+// Close method triggers graceful shutdown on the task. It should block till task is properly closed.
+// When Close timeout is exceeded then given context is canceled.
+func (l *BaseLooper) Close(ctx context.Context) error {
+	return l.runner.Close(ctx)
+}
+
+func NewBaseLooper(looper func(ctx context.Context, loop *Loop) error) *BaseLooper {
+	return &BaseLooper{
+		runner: Looper(looper),
+	}
+}
